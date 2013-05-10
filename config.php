bd58a6f2007c9d3bacada034dd1144b86767c064
Chetan Chopra <chetan.rcc26@gmail.com>
2013-05-10 17:51:48 +0530

    GitHub
    commit to github

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?
 //facebook application
-$fbconfig['appid' ] = "";  // write your application id
-$fbconfig['secret'] = ""; // write your api key
-$fbconfig['baseUrl'] = "http://evoluzando.com/products/meme-generator/"; //for example: http://evoluzando.com/apps/geradorui/
-$fbconfig['appBaseUrl'] = "http://apps.facebook.com/meme-generator-app"; //for example: http://apps.facebook.com/evoluzando/
+$fbconfig['appid' ] = "333620350098687";  // write your application id
+$fbconfig['secret'] = "b72868a7249a59b40d7e2da066eadd7d"; // write your api key
+$fbconfig['baseUrl'] = "https://meme-maker-for-babies.herokuapp.com/"; //for example: http://evoluzando.com/apps/geradorui/
+$fbconfig['appBaseUrl'] = "http://apps.facebook.com/baby-meme"; //for example: http://apps.facebook.com/evoluzando/
 $message = 'Created with Meme-generator: '.$fbconfig['appBaseUrl'];
 $memeImg = 'meme2.jpg'; // enter the name of the image of the meme
 $textPosition1 = '50% 4%'; // horizontal vertical
